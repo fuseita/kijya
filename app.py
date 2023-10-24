e237e545a25814e59d949e893b7a9358c0782de7
Po-Yang Chen <poyang31@yahoo.com.tw>
2023-10-24 09:25:57 +0000

    add features

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from os import getcwd, mkdir
+from os import getcwd, mkdir, remove
 from os.path import join, exists
+
+from yaml import full_load
+from zipfile import ZipFile
 
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = join(getcwd(), "upload")
-ALLOWED_EXTENSIONS = {'zip'}
-
-if not exists(UPLOAD_FOLDER):
-    mkdir(UPLOAD_FOLDER)
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+config_path = join(getcwd(), "config.yaml")
+app.config.from_file(config_path, load=full_load)
+
+if not exists(app.config["UPLOAD_FOLDER"]):
+    mkdir(app.config["UPLOAD_FOLDER"])
 
 
 def file_extention(filename) -> str | None:
     if '.' not in filename:
         return None
     return filename.rsplit('.', 1)[1].lower()
+
 
 @app.route('/', methods=['GET'])
 def upload_file_page():
@@ -30,17 +33,25 @@ def upload_file_page():
             <body>
                 <h1>Upload zip file</h1>
                 <form method="post" enctype="multipart/form-data">
-                    <input type="file" name="file" />
+                    <input type="text" name="path" required placeholder="Path" />
+                    <input type="text" name="password" required placeholder="Password" />
+                    <input type="file" name="file" required accept=".zip" />
                     <input type="submit" value="Upload" />
                 </form>
             </body>
         </html>
     '''
 
+
 @app.route('/', methods=['POST'])
 def upload_file_handler():
-    if 'file' not in request.files:
+    if 'path' not in request.form \
+        or 'password' not in request.form \
+            or 'file' not in request.files:
         return "bad request", 400
+
+    if request.form.get('password') != app.config["SECRET_KEY"]:
+        return "unauthorized", 401
 
     file = request.files['file']
     if not file or file.filename == '':
@@ -49,8 +60,14 @@ def upload_file_handler():
     if file_extention(file.filename) != 'zip':
         return "bad request", 400
 
-    dist_filename = secure_filename(file.filename)
-    dist_filepath = join(app.config['UPLOAD_FOLDER'], dist_filename)
-    file.save(dist_filepath)
+    zip_filename = secure_filename(file.filename)
+    zip_filepath = join(app.config['UPLOAD_FOLDER'], zip_filename)
+    file.save(zip_filepath)
 
+    with ZipFile(zip_filepath, mode='r') as zf:
+        name_list = zf.namelist()
+        for name in name_list:
+            zf.extract(name, request.form.get("path"))
+
+    remove(zip_filepath)
     return "received", 201
