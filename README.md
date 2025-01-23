5cf4afa67173ad32a7df4c65c3bc6794933d4cc3
Po-Yang Chen <poyang31@yahoo.com.tw>
2025-01-23 12:20:13 +0800

    fastapi

---
+++ README.md
@@ -0,0 +1,51 @@
+# Kijya
+
+究極簡單的伺服器更新管理程式，利用 ZIP壓縮檔 升級伺服器程式。~~反正程式能動就好~~
+
+https://github.com/fuseita/kijya
+
+把這個檔案放到GitHub的專案裡面，就能讓程式神奇的自己更新自己本身。~~雖然走走還是看不懂這個程式怎麼動的~~
+
+```yaml
+name: 自動化更新系統 🖧
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  run-kijya:
+    runs-on: ubuntu-latest
+    steps:
+      - name: 載入 Git 檔案 🛎️
+        uses: actions/checkout@v2.3.1
+          
+      - name: 使用 Zip 📤
+        uses: montudor/action-zip@v1
+
+      - name: 製作 Zip 📥
+        run: zip -qq -r build.zip .
+
+      - name: 上傳 Zip 🚀
+        uses: JantHsueh/upload-file-action@master
+        with:
+          url: ${{ vars.KIJYA_URL }}
+          forms: '{"password":"${{ secrets.KIJYA_PASS }}","path":"${{ vars.KIJYA_PATH }}","cmd":"${{ vars.KIJYA_CMD }}"}'
+          fileForms: '{"file":"build.zip"}'
+```
+
+走走習慣使用[supervisord](http://supervisord.org)架設伺服器，這個設定檔你可以拿去用。~~反正他不知道我從伺服器偷出來給你用~~
+
+```ini
+[program:kijya]
+command=/server/kijya/venv/bin/fastapi run --port 8100
+directory=/server/kijya
+user=server
+startsecs=0
+stopwaitsecs=0
+autostart=true
+autorestart=true
+stdout_logfile=/server/kijya/stdout.log
+stderr_logfile=/server/kijya/error.log
+```
---
+++ app.py
@@ -0,0 +1,105 @@
+from os import getcwd, remove, system
+from os.path import join
+
+from hmac import compare_digest
+
+from yaml import safe_load
+from zipfile import ZipFile
+
+from fastapi import FastAPI, Form, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse, PlainTextResponse
+
+from typing import Optional
+
+app = FastAPI()
+
+config_path = join(getcwd(), "config.yaml")
+with open(config_path, "r") as config_file:
+    config = safe_load(config_file)
+
+
+def safe_compare(a: str, b: str) -> bool:
+    a = a.encode("utf-8")
+    b = b.encode("utf-8")
+    return compare_digest(a, b)
+
+
+def file_extension(filename: str) -> Optional[str]:
+    if "." not in filename:
+        return None
+    return filename.rsplit(".", 1)[1].lower()
+
+
+@app.get("/", response_class=HTMLResponse)
+def index_page():
+    return '''
+        <!doctype html>
+        <html>
+            <head>
+                <title>Kijya</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+            </head>
+            <body>
+                <div class="px-4 py-5 my-5 text-center">
+                    <h1 class="display-5 text-body-emphasis">Kijya</h1>
+                    <p class="lead mb-4">利用 ZIP壓縮檔 升級伺服器程式</p>
+                    <form method="post" enctype="multipart/form-data" class="mx-auto p-4 p-md-5 border rounded-3 bg-body-tertiary">
+                        <div class="form-floating mb-3">
+                            <input type="text" name="path" required id="input-path" placeholder="Path" class="form-control" />
+                            <label for="input-path">Path</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" name="password" required id="input-password" placeholder="Password" class="form-control" />
+                            <label for="input-password">Password</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" name="cmd" id="input-cmd" placeholder="Command" class="form-control" />
+                            <label for="input-cmd">Command</label>
+                        </div>
+                        <div class="mb-3">
+                            <input type="file" name="file" required accept=".zip" class="form-control" />
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="submit" value="Upload" class="w-100 btn btn-lg btn-primary" />
+                        </div>
+                    </form>
+                </div>
+            </body>
+        </html>
+    '''
+
+
+@app.get("/robots.txt", response_class=PlainTextResponse)
+def robots_txt():
+    return "User-agent: *\nDisallow: /"
+
+
+@app.post("/")
+async def upload_zip(
+    path: str = Form(...),
+    password: str = Form(...),
+    cmd: Optional[str] = Form(None),
+    file: UploadFile = Form(...),
+):
+    if not (config["SECRET_KEY"] and safe_compare(password, config["SECRET_KEY"])):
+        raise HTTPException(status_code=401, detail="wrong password")
+
+    if file_extension(file.filename) != "zip":
+        raise HTTPException(status_code=400, detail="file is not a zip")
+
+    zip_filepath = file.filename
+    with open(zip_filepath, "wb") as f:
+        f.write(await file.read())
+
+    with ZipFile(zip_filepath, mode="r") as zf:
+        for name in zf.namelist():
+            zf.extract(name, path)
+
+    remove(zip_filepath)
+
+    if cmd:
+        system(cmd)
+        return {"message": "received and loaded"}
+    else:
+        return {"message": "received"}
